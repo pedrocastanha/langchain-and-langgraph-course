30ea987b54fe3cdd79e78770f1df57e0c78de8f6
Pedro CAstanheira Costa <pedrinho.castanha@gmail.com>
2025-10-20 21:55:18 -0300

    lesson 34

--- section-4/main.py
+++ section-4/main.py
@@ -1,56 +1,86 @@
-import os
-from typing import List, Union
+from typing import Union, List
+
 from dotenv import load_dotenv
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain_core.agents import AgentAction, AgentFinish
 from langchain_core.prompts import PromptTemplate
-from langchain_core.tools import Tool, render_text_description
+from langchain_core.tools import tool, Tool, render_text_description
 from langchain_openai import ChatOpenAI
 
 load_dotenv()
-from langchain.agents import tool
 
 @tool
-def get_text_length(text:str)-> int:
-    """ Returns the length of a text by characters """
-    print(f"get_text_length enter with {text}")
+def get_text_length(text: str) -> int:
+    """Returns the length of a text by characters"""
+    print(f"get_text_length enter with {text=}")
     text = text.strip("'\n").strip(
-      '"'
-    )
+        '"'
+    )  # stripping away non alphabetic characters just in case
+
     return len(text)
 
-def find_tool_by_name(tools: List[Tool], tool_name:str) -> Tool:
+
+def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
     for tool in tools:
         if tool.name == tool_name:
             return tool
-    raise ValueError(f"Tool with name {tool_name} not found")
+    raise ValueError(f"Tool wtih name {tool_name} not found")
 
-if __name__ == '__main__':
-        print("Hello ReAct LangChain!")
-        tools = [get_text_length]
 
-        template = """
-            Answer the following questions as best you can. You have access to the following tools:
+if __name__ == "__main__":
+    print("Hello ReAct LangChain!")
+    tools = [get_text_length]
 
-            {tools}
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
 
-            Use the following format:
+    {tools}
 
-            Question: the input question you must answer
-            Thought: you should always think about what to do
-            Action: the action to take, should be one of [{tool_names}]
-            Action Input: the input to the action
-            Observation: the result of the action
-            ... (this Thought/Action/Action Input/Observation can repeat N times)
-            Thought: I now know the final answer
-            Final Answer: the final answer to the original input question
+    Use the following format:
 
-            Begin!
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
 
-            Question: {input}
-            Thought:
-            """
-        prompt = PromptTemplate.from_template(template=template).partial(
-            tools = render_text_description(tools),
-            tool_names = ", ".join([t.name for t in tools]),
-        )
+    Begin!
 
-        llm = ChatOpenAI(temperature=0, stop=["\nObservation", "Observation"], openai_api_key = os.getenv("OPENAI_API_KEY"))
+    Question: {input}
+    Thought: {agent_scratchpad}
+    """
+
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools),
+        tool_names=", ".join([t.name for t in tools]),
+    )
+
+llm = ChatOpenAI(temperature=0, stop = ["\nObservation", "Observation"])
+intermediate_steps = []
+agent = (
+    {
+        "input": lambda x: x["input"],
+        "agent_scratchpad": lambda x: x["agent_scratchpad"]
+    }
+    | prompt
+    | llm
+    | ReActSingleInputOutputParser()
+)
+
+agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+    {
+        "input": "What's the length of 'DOG' in characters?"
+    }
+)
+print(agent_step)
+
+if isinstance(agent_step, AgentAction):
+        tool_name = agent_step.tool
+        tool_to_use = find_tool_by_name(tools, tool_name)
+        tool_input = agent_step.tool_input
+
+        observation = tool_to_use.func(str(tool_input))
+        print(f"{observation=}")
