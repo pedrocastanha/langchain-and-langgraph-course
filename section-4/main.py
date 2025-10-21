9698d81657bcd0a937cbcc1f06cfe0713197d0f1
Pedro CAstanheira Costa <pedrinho.castanha@gmail.com>
2025-10-20 22:17:01 -0300

    lesson 36

--- section-4/main.py
+++ section-4/main.py
@@ -1,11 +1,12 @@
-from typing import Union, List
-
+from typing import List, Union
 from dotenv import load_dotenv
+from langchain.agents.format_scratchpad import format_log_to_str
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from langchain_core.agents import AgentAction, AgentFinish
 from langchain_core.prompts import PromptTemplate
-from langchain_core.tools import tool, Tool, render_text_description
+from langchain_core.tools import Tool, render_text_description, tool
 from langchain_openai import ChatOpenAI
+from callbacks import AgentCallbackHandler
 
 load_dotenv()
 
@@ -15,8 +16,7 @@ def get_text_length(text: str) -> int:
     print(f"get_text_length enter with {text=}")
     text = text.strip("'\n").strip(
         '"'
-    )  # stripping away non alphabetic characters just in case
-
+    )
     return len(text)
 
 
@@ -58,44 +58,40 @@ if __name__ == "__main__":
         tool_names=", ".join([t.name for t in tools]),
     )
 
-llm = ChatOpenAI(temperature=0, stop = ["\nObservation", "Observation"])
-intermediate_steps = []
-agent = (
-    {
-        "input": lambda x: x["input"],
-        "agent_scratchpad": lambda x: x["agent_scratchpad"]
-    }
-    | prompt
-    | llm
-    | ReActSingleInputOutputParser()
-)
-
-agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+    llm = ChatOpenAI(
+        temperature=0,
+        stop=["\nObservation", "Observation"],
+        callbacks=[AgentCallbackHandler()],
+    )
+    intermediate_steps = []
+    agent = (
         {
-            "input": "What is the length of 'DOG' in characters?",
-            "input": "What is the length of the word: DOG",
-            "agent_scratchpad": intermediate_steps,
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_log_to_str(x["agent_scratchpad"]),
         }
+        | prompt
+        | llm
+        | ReActSingleInputOutputParser()
     )
 
-print(agent_step)
+    agent_step = ""
+    while not isinstance(agent_step, AgentFinish):
+        agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+            {
+                "input": "What is the length of the word: DOG",
+                "agent_scratchpad": intermediate_steps,
+            }
+        )
+        print(agent_step)
 
-if isinstance(agent_step, AgentAction):
-    tool_name = agent_step.tool
-    tool_to_use = find_tool_by_name(tools, tool_name)
-    tool_input = agent_step.tool_input
+        if isinstance(agent_step, AgentAction):
+            tool_name = agent_step.tool
+            tool_to_use = find_tool_by_name(tools, tool_name)
+            tool_input = agent_step.tool_input
 
-    observation = tool_to_use.func(str(tool_input))
-    print(f"{observation=}")
-    intermediate_steps.append((agent_step, str(observation)))
+            observation = tool_to_use.func(str(tool_input))
+            print(f"{observation=}")
+            intermediate_steps.append((agent_step, str(observation)))
 
-agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
-    {
-        "input": "What is the length of the word: DOG",
-        "agent_scratchpad": intermediate_steps,
-    }
-)
-print(agent_step)
-if isinstance(agent_step, AgentFinish):
-    print("### AgentFinish ###")
-    print(agent_step.return_values)
+        if isinstance(agent_step, AgentFinish):
+            print(agent_step.return_values)
